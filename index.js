f9dc34a6447ca1776419deef1b472ea3fb6d8485
Vincent <vincent.makokha@student.moringaschool.com>
2022-08-21 19:01:27 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name, email){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            accept: "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.querySelector('body').innerHTML = `<p>id: ${data.id}`;
+    })
+    .catch(error => {
+        alert('an error has occured')
+        document.querySelector('body').innerHTML += `<p>${error.message} </p>`
+    })
+}
+
+submitData("Steve", "steve@steve.com")
