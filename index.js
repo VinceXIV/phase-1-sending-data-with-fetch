33d7afea417087b9ff759129a95f8b0e2edeb04e
Vincent <vincent.makokha@student.moringaschool.com>
2022-08-26 12:00:33 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
 // Add your code here
-function submitData(name, email){
-    return fetch("http://localhost:3000/users", {
+const domForm = document.createElement('form')
+domForm.innerHTML = `<label for="name">Name</label>
+    <input type="text" name="name" id="name">
+    <label for="email">Email</label>
+    <input type="email" name="email" id="email">
+    <input type="submit" value="submit">
+    `
+
+const listOfNamesAndEmails = document.createElement('table')
+listOfNamesAndEmails.id = 'names-and-emails'
+listOfNamesAndEmails.innerHTML = `<thead>
+</thead>
+<tbody>
+</tbody>`
+
+
+document.querySelector('body').append(domForm)
+document.querySelector('body').append(listOfNamesAndEmails)
+
+
+function getFormData(form){
+    return {
+        name: form.querySelector('#name').value,
+        email: form.querySelector('#email').value
+    }
+}
+
+function submitData(data){
+    fetch("http://localhost:3000/users", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
             accept: "application/json"
         },
-        body: JSON.stringify({
-            name: name,
-            email: email
-        })
+        body: JSON.stringify(data)
     })
     .then(response => response.json())
     .then(data => {
-        document.querySelector('body').innerHTML = `<p>id: ${data.id}`;
+        console.log("data", data)
+        domForm.reset()
+        document.querySelector('#names-and-emails tbody')
+        .innerHTML += `<tr style="padding: 15px">
+                    <td style="padding: 0 15px">${data.name}</td>
+                      <td style="padding:0 15px">${data.email}</td>
+                      </tr>`
     })
     .catch(error => {
         alert('an error has occured')
@@ -21,4 +51,7 @@ function submitData(name, email){
     })
 }
 
-submitData("Steve", "steve@steve.com")
+domForm.addEventListener('submit', e=>{
+    e.preventDefault()
+    submitData(getFormData(e.target))
+})
